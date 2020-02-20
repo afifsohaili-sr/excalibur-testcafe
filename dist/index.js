306e6cffe4df80efc9ca4c078e33ed51a1d3d66b
Afif Sohaili <afifnajib@gmail.com>
2020-02-20 22:54:10 +0800

    Publish /dist folder.

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var excalibur_1 = require("./src/excalibur");
+exports.Excalibur = excalibur_1.Excalibur;
+var pluginManager_1 = require("./src/pluginManager");
+exports.PluginManager = pluginManager_1.PluginManager;
---
+++ src/excalibur.js
@@ -0,0 +1,28 @@
+"use strict";
+exports.__esModule = true;
+var axios_1 = require("axios");
+var Excalibur = /** @class */ (function () {
+    function Excalibur(_a) {
+        var _b = _a.confluenceUrl, confluenceUrl = _b === void 0 ? 'localhost:8090' : _b, _c = _a.confluenceUrlPrefix, confluenceUrlPrefix = _c === void 0 ? '' : _c, _d = _a.password, password = _d === void 0 ? 'admin' : _d, _e = _a.username, username = _e === void 0 ? 'admin' : _e;
+        this.confluenceUrl = confluenceUrl;
+        this.confluenceUrlPrefix = confluenceUrlPrefix;
+        this.password = password;
+        this.username = username;
+    }
+    Excalibur.prototype.getHttpService = function () {
+        if (this.httpService) {
+            return this.httpService;
+        }
+        var httpService = axios_1["default"].create({
+            baseURL: "http://" + this.confluenceUrl + this.confluenceUrlPrefix,
+            auth: {
+                username: this.username,
+                password: this.password
+            }
+        });
+        this.httpService = httpService;
+        return httpService;
+    };
+    return Excalibur;
+}());
+exports.Excalibur = Excalibur;
